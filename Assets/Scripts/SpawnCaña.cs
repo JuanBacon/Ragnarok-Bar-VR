a038ecf7d1c2fbf50b4b86a26383b225ac796398
CamiloReyes <ca.milo44@hotmail.com>
2020-06-20 17:24:48 -0500

    Scena con Pez y Lanza
    se creo una scena donde estan configurados los peces y las lanzas

---
+++ Assets/Scripts/SpawnCaña.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCaña : MonoBehaviour
+{
+
+    public GameObject objPre;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(objPre,transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
