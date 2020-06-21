b54bc1867b0f5f5e51692561f209f76e7ed42fc5
CamiloReyes <ca.milo44@hotmail.com>
2020-06-20 20:22:10 -0500

    Escena con Juego y mov
    Falta añadir mas peces y poner la camara

---
+++ Assets/Scripts/ScriptsPesca/MovRandom.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovRandom : MonoBehaviour
+{
+
+    public Vector3 NewPos;
+    
+    public float SegundosMov= 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        SegundosMov -= Time.deltaTime;
+
+        if(SegundosMov<0)
+        {
+        NewPos = new Vector3(Random.insideUnitSphere.x*1.2f,0,Random.insideUnitSphere.z*1.2f);
+        SegundosMov= 3f;
+        }
+        transform.Translate(NewPos*Time.deltaTime);
+        
+    }
+
+
+}
